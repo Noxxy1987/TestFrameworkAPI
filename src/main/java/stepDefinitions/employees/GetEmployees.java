25a8f306fe25e8a8542980be17a613743ca06e3d
Hunor <smorocz-ext@pentalog.com>
2022-02-01 15:48:02 +0200

    get on employee

--- src/main/java/stepDefinitions/employees/GetEmployees.java
+++ src/main/java/stepDefinitions/employees/GetEmployees.java
@@ -1,5 +1,9 @@
 package stepDefinitions.employees;
 
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.en.Then;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import utils.Logger;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -8,8 +12,14 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import utils.TestContext;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,18 +28,55 @@ public class GetEmployees {
     public static final String EMPLOYEE_URL = "http://dummy.restapiexample.com/api/v1/employees";
 
     @When("^Get request on v1/employees$")
-     public void getRequest() {
-        Response response = get( EMPLOYEE_URL);
-        Logger.log(response.getBody().asString());
+    public void getRequest() {
+        Response response = get(EMPLOYEE_URL);
+        Logger.log("response: ", response.getBody().asString());
         TestContext.INSTANCE.add("response", response);
 //        Assertions.assertEquals(200, response.getStatusCode());
     }
 
-    @When("^Response is (.*)$")
-    public void assertGetRequest(String code) {
+    @Then("^Response code is (.*)$")
+    public void assertResponseCode(Integer code) {
         Response response = (Response) TestContext.INSTANCE.get("response");
-//        assertThat();
+        assertThat(String.format("Invalid response code! Expected: %s, Actual: %s", code, response.getStatusCode()), response.getStatusCode() == code, is(true));
     }
 
+    @Then("^Response schema corresponds with baseline: (.*)$")
+    public void assertResponseSchema(String baseline) {
+        Response response = (Response) TestContext.INSTANCE.get("response");
 
+        response.then().assertThat()
+                .body(matchesJsonSchemaInClasspath(baseline));
+    }
+
+    @Then("^Response body corresponds with baseline: (.*)$")
+    public void assertResponseBody(String baseline) {
+        Response response = (Response) TestContext.INSTANCE.get("response");
+
+
+    }
+
+    @When("^Response headers are returned:")
+    public void assertResponseHeaders(Map<String, String> headers) {
+        Response response = (Response) TestContext.INSTANCE.get("response");
+        StringBuilder errormessage = new StringBuilder();
+
+        Map<String, String> actualheaders = response.getHeaders().asList().stream().sorted(Comparator.comparing(Header::getName)).collect(Collectors.toMap(Header::getName, Header::getValue, (v1, v2) -> v2));
+//        Boolean identical = headers.entrySet().stream().allMatch(e -> e.getValue().equals(actualheaders.get(e.getKey())));
+
+        assertThat(String.format("Expected headers size: %s, Actual: %s", headers.size(), actualheaders.entrySet().size()), headers.size() == actualheaders.entrySet().size(), is(true));
+        for (String key : headers.keySet()) {
+
+            if (!headers.get(key).equals(actualheaders.get(key))&&(!("ExpiresDate").contains(key))) {
+                errormessage.append(String.format("invalid header value, Expected: %s, Actual: %s", headers.get(key), actualheaders.get(key)));
+            }
+
+        }
+        assertThat(String.format("Failed: %s", errormessage.toString()), errormessage.toString().equals(""), is(true));
+    }
+
+    @DataTableType(replaceWithEmptyString = "[blank]")
+    public String stringType(String cell) {
+        return cell;
+    }
 }
--- src/test/resources/features/getEmployees.feature
+++ src/test/resources/features/getEmployees.feature
@@ -1,5 +1,29 @@
 Feature: Get Employees
 
-  @Test1
+  @Scenario1
   Scenario: GET Employee endpoint
     When Get request on v1/employees
+    Then Response code is 200
+    And Response headers are returned:
+      | Cache-Control          | no-cache, private, max-age=31536000 |
+      | Content-Encoding       | gzip                                |
+      | Content-Length         | 631                                 |
+      | Content-Type           | application/json                    |
+      | Date                   | Tue, 01 Feb 2022 08:03:14 GMT       |
+      | Display                | staticcontent_sol                   |
+      | Expires                | Wed, 01 Feb 2023 08:03:14 GMT       |
+      | Host-Header            | c2hhcmVkLmJsdWVob3N0LmNvbQ==        |
+      | Referrer-Policy        | [blank]                             |
+      | Response               | 200                                 |
+      | Server                 | nginx                               |
+      | Vary                   | Accept-Encoding,User-Agent,Origin   |
+      | X-Ezoic-Cdn            | Miss                                |
+      | X-Middleton-Display    | staticcontent_sol                   |
+      | X-Middleton-Response   | 200                                 |
+      | X-Origin-Cache-Control | no-cache, private                   |
+      | X-Ratelimit-Limit      | 60                                  |
+      | X-Ratelimit-Remaining  | 58                                  |
+      | X-Server-Cache         | false                               |
+      | X-Sol                  | pub_site                            |
+    And Response schema corresponds with baseline: baselines/getEmployeeSchema.json
+    And Response body corresponds with baseline: baselines/getEmployeeSchema.json
